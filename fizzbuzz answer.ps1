6227eabfcc96f48fb8302bd64f1205c345b1e38f
Kyle Mould <53817087+KyleMould@users.noreply.github.com>
2019-08-08 10:49:00 +0100

    Fizz Buzz answer added via web upload
    Powershell answer to the apparently popular FizzBuzz question asked in coding interviews. 
    
    I'm pretty unlikely to go for these jobs, but i worked this out.

---
+++ fizzbuzz answer.ps1
@@ -0,0 +1,20 @@
+﻿1..100| foreach-object { #loop 100 numbers
+
+    
+   $output = '' #placeholder
+
+   if ($_% 3 -eq 0)
+   {
+        $output += "fizz" #divide by 3
+   }
+if ($_%5 -eq 0)
+{
+    $output += "Buzz" #Divide by 5 
+}
+if ($output -eq '')
+{
+    $output = $_ #if either. just the number not fizz or buzz
+}
+$output
+}
+
